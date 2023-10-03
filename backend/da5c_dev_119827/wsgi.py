0d2af15f52ed764d1699629d8de3fd63e0cb7e0d
Crowdbotics <engineering@crowdbotics.com>
2023-10-03 11:19:37 +0000

    Initial commit

---
+++ backend/da5c_dev_119827/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for da5c_dev_119827 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "da5c_dev_119827.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "da5c-dev-119827",
+  url: "https://da5c-dev-119827.botics.co",
+  api: "https://da5c-dev-119827.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
